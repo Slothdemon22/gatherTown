5a2368b02e7a040edadc82e32a95bf05a98ae9aa
Slothdemon <www.basilslothdemon@gmail.com>
2025-03-14 16:06:30 +0500

    prettierrc file added

--- backend/middlewares/authMiddleware.ts
+++ backend/middlewares/authMiddleware.ts
@@ -7,17 +7,21 @@ export const authMiddleware = (req: AuthRequest, res: Response, next: NextFuncti
     const token = req.header("Authorization")?.split(" ")[1];
     
     if (!token) {
-         res.status(401).json({ message: "Unauthorized access" });
+         res.status(401).json({ message: "Unauthorized access" ,status:401});
          return;
     }
     
     try {
         
         const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+        if (!decoded)
+        res.status(401).json({ message: "Unauthorized access" ,status:401});
+        
+        
         req.user = decoded;  
         next();
     } catch (error) {
-         res.status(401).json({ message: "Invalid token" });
+        res.status(401).json({ message: "Unauthorized access" ,status:401});
          return;
     }
 };
--- backend/models/testModel.js
+++
@@ -1,3 +0,0 @@
-console.log("basil made a pull request");
-console.log("Second feature");
-console.log("merge test");
